af1a2aa1c38218753b1d7da61eb3f840b6b831fd
aloyhenry <henryaloysius4@gmail.com>
2023-01-09 10:22:28 +0100

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,4 +13,8 @@ class Square(Rectangle):
         """Checks and sets the default attributes of Square class."""
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = 
+        self.__size = size
+
+    def __str__(self):
+        """Sets the str behaviour."""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
